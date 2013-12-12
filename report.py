0544247fa173e39da5da6fe3bb38bf501138c871
Arpad Kovacs <minhmeoke@gmail.com>
2013-12-11 22:28:40 -0800

    Add Daniel's changes

--- report.py
+++ report.py
@@ -25,7 +25,7 @@ stopwords_df.columns=['word','index']
 stopwords=set('word:'+stopwords_df['index'])
 
 df = pd.read_table('data/new_chats_dataset.csv', sep=';', header=None, nrows=N+M+1)
-df.columns=['chatid','user1','user2','profile1','profile2','start','end','disconnector','reporteduser','reportedreason','numlines1','numlines2','words1','words2','friends']
+df.columns=['chatid','user1','user2','profile1','profile2','start','end','disconnector','reporteduser','reportedreason','numlines1','numlines2','words1','words2']
 df_test = df[N+1:N+M+1]
 df = df[:N]
 
@@ -71,6 +71,9 @@ def parse(df):
     df['u1al'] = df.about1.apply(len)
     df['u2al'] = df.about2.apply(len)
 
+    df.location1[df.location1.isnull()]=''
+    df.location2[df.location2.isnull()]=''
+
     # compute average conversation length for each user: (sum numlines)/(count conversations)
     for user in ('1','2'):
       g = df.ix[:,['user'+user,'numlines'+user]].groupby('user'+user)
@@ -99,7 +102,7 @@ def extract(df):
         df.ix[:,['age1','gender1']].astype(int).values,
         df.ix[:,['age2','gender2']].astype(int).values,
         df.ix[:,['avg_conv1','avg_conv2']].values,
-        df.ix[:,['about_empty1','about2_empty']].values,
+        df.ix[:,['about1_empty','about2_empty']].values,
         df.ix[:,['gender_eq', 'age_diff']].values,
         df.ix[:,['u1al', 'u2al']].values,
         hstack([d.fit_transform(df.ix[:,'words1']),
@@ -146,26 +149,13 @@ from sklearn import cross_validation
 #print cross_validation.cross_val_score(clf, Xs[0], y_train, cv=10,
         #scoring='f1', verbose=2, n_jobs=8)
 
-# predict the friends 'f' field
-df['u1'] = df['user1'].apply(lambda s: int(s.split(':')[1]))
-df['u2'] = df['user2'].apply(lambda s: int(s.split(':')[1]))
-df['p1'] = df['profile1'].apply(lambda s: int(s.split(':')[1]))
-df['p2'] = df['profile2'].apply(lambda s: int(s.split(':')[1]))
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-df['f'] = le.fit_transform(df['friends'])
-dd=df.ix[:,['u1','u2','p1','p2','age','gender','location_flag',
-    'u1al','age2','gender2','location_flag2','u2al','age_diff',
-    'gender_eq', 'f']].astype(int)
-#cross_validation.cross_val_score(clf, dd.ix[:,:14], dd.ix[:,14], cv=10,
-        #scoring='f1', n_jobs=5)
-
 # predict user quality
 users = pd.read_table('data/new_users_dataset.csv', sep=';', header=None,
         index_col=0)
 users.columns = ['quality']
-dfq = df.join(users, on=1, how='inner')
+dfq = df.join(users, on='user1', how='inner')
 dfq.quality = (dfq.quality=='clean').astype(int)
 Xq, Xqs = extract(dfq)
-gs.fit(Xq, dfq.quality.values)
+# don't use the user columns for predicting the user quality
+gs.fit(hstack(Xqs[2:]), dfq.quality.values)
 print gs.best_score_
